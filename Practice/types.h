73f64da42ba130985f9b9e8c2f23e14d001e50ed
John Andrew Chua <thisischuaa30@gmail.com>
2025-09-17 23:07:36 +0800

    09172025 (3)
    ADTS w bryce

---
+++ Practice/types.h
@@ -0,0 +1,45 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+typedef struct {
+    int arr[MAX];
+    int count;
+} List;
+
+void initialize (List* L){
+    L->count = 0;
+}
+
+void insertFirst(List* L, int data){
+    for (int i = L->count; i > 0; i--){
+        L->arr[i] = L->arr[i - 1];
+    }
+    L->arr[0] = data;
+    L->count++;
+}
+
+void insertLast(List *L, int data){
+    L->arr[L->count++] = data;
+}
+
+void insertAtIndex(List *L, int data, int position){
+    for (int i = L->count; i > position; i--){
+        L->arr[i] = L->arr[i-1];
+    }
+    L->arr[position] = data;
+    L->count++;
+}
+
+void display(List* L, int data){
+    printf("Array:");
+    for (int i = 0; i < L->count; i++){
+        printf("[%d]", L->arr[i]);
+    }
+}
+#endif
