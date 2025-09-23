ef9fa09034f3576499782e51104152c854aabef6
John Andrew Chua <thisischuaa30@gmail.com>
2025-09-23 23:29:24 +0800

    Update helpers.c
    this is hopeless man

--- Exams/[E1] DSA - Plane Deboarding Strategy/helpers.c
+++ Exams/[E1] DSA - Plane Deboarding Strategy/helpers.c
@@ -4,16 +4,8 @@
 #include <string.h>
 #include "types.h"
 
-// TODO: Write helper functions here...
-
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "types.h"
-
 // =============================
-// Stack V1 (ABC, bottom is A)
+// Stack V1 (ABC, bottom is A, top grows upward) this shit is hopeless
 // =============================
 void initializeStack_v1(Group* g) {
     g->top = -1;
@@ -27,7 +19,7 @@ bool isEmpty_v1(Group* g) {
     return g->top == -1;
 }
 
-void push_v1(Group* g, char* value) {
+void push_v1(Group* g, const char* value) {
     if (isFull_v1(g)) {
         printf("Stack V1 full! Cannot push %s\n", value);
         return;
@@ -46,28 +38,28 @@ void pop_v1(Group* g) {
 }
 
 // =============================
-// Stack V2 (DEF, bottom is F)
+// Stack V2 (DEF, bottom is F, top grows upward)
 // =============================
 void initializeStack_v2(Group* g) {
-    g->top = 0; // empty means 0 passengers
+    g->top = -1;   // same convention as v1 (empty = -1)
 }
 
 bool isFull_v2(Group* g) {
-    return g->top == MAX_PASSENGERS;
+    return g->top == MAX_PASSENGERS - 1;
 }
 
 bool isEmpty_v2(Group* g) {
-    return g->top == 0;
+    return g->top == -1;
 }
 
-void push_v2(Group* g, char* value) {
+void push_v2(Group* g, const char* value) {
     if (isFull_v2(g)) {
         printf("Stack V2 full! Cannot push %s\n", value);
         return;
     }
+    g->top++;
     strncpy(g->p[g->top].passengerName, value, sizeof(g->p[g->top].passengerName) - 1);
     g->p[g->top].passengerName[sizeof(g->p[g->top].passengerName) - 1] = '\0';
-    g->top++;
 }
 
 void pop_v2(Group* g) {
